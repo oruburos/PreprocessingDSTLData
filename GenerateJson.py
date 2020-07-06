4560115de2878552aab515aafc236af522dac18b
oruburos <omar.verduga@gmail.com>
2020-07-06 14:57:08 +0100

    Data preprocessors in Python for Experiment 1

--- GenerateJson.py
+++ GenerateJson.py
@@ -19,7 +19,7 @@ mysqlServer = 'mysql+pymysql://root:@localhost:3308/dstlProlific'
 engine = create_engine(mysqlServer)
 
 tableTrials = pd.read_sql_query(
-    'SELECT users_prolific_expandedv5.*,expanded.*,prolificdata.* FROM participant_category_learning as categorylearning,   users_prolific_expandedv5, prolificdata,participantpractice as practice,	participantmonitoring_secondround as monitoring1, 	participantmonitoring as monitoring2, 	experiment as expanded where 	users_prolific_expandedv5.prolific_id = prolificdata.participant_id	and  	users_prolific_expandedv5.id_participant = categorylearning.id  	and 	users_prolific_expandedv5.id_participant = practice.id 	and	users_prolific_expandedv5.id_participant =  monitoring1.id  	and users_prolific_expandedv5.id_participant = monitoring2.id	and 	prolificdata.status = "APPROVED" and users_prolific_expandedv5.completed = 1'
+    'SELECT users_prolific_expandedv5.*,experiment.*, prolificdata.* from users_prolific_expandedv5, prolificdata, experiment WHERE users_prolific_expandedv5.prolific_id =prolificdata.participant_id and users_prolific_expandedv5.completed = 1 and users_prolific_expandedv5.id_participant = experiment.id_participant'
     , engine)
-tableTrials.to_csv("CompleteTrial.csv", index=False);
-tableTrials.to_sql("completetrials", con= engine ,if_exists ='append' , index= False)
+tableTrials.to_csv("CompleteTrialExp1.csv", index=False);
+tableTrials.to_sql("completetrialsexp1", con= engine ,if_exists ='append' , index= False)
